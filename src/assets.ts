b0f0c0e4902c02c588eb689fcd2ac49c1de13972
Matt Jennings <me@mattjennings.io>
2023-06-21 20:38:20 -0500

    fix asset urls

--- src/assets.ts
+++ src/assets.ts
@@ -1,8 +1,8 @@
 // I opted for prefixing all assets with type_name rather than
 // nesting but I'm find to nest them too. i.e assets.images.level1 etc
 export const assets = {
-  img_level1: new ex.ImageSource('/maps/level1.png'),
-  img_player: new ex.ImageSource('/sprites/player.png'),
+  img_level1: new ex.ImageSource('maps/level1.png'),
+  img_player: new ex.ImageSource('sprites/player.png'),
 
   // snd_jump: ... etc
 }
