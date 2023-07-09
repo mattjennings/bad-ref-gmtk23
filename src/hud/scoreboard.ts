53932eb0976568911dc30ae83e25f74896990083
Drew Conley <drewconley@gmail.com>
2023-07-08 22:44:21 -0600

    add yep

---
+++ src/hud/scoreboard.ts
@@ -0,0 +1,11 @@
+import { ScreenElement } from "excalibur";
+import {assets} from "../assets";
+
+export class Scoreboard extends ScreenElement {
+    constructor() {
+        super({
+            x: 4,
+            y: 180
+        });
+    }
+}
