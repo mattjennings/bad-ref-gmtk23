7ba8abf31052809f4aa18515b256ff21c07600a5
Drew Conley <drewconley@gmail.com>
2023-07-08 23:24:26 -0600

    add drew/hud

--- src/hud/scoreboard.ts
+++ src/hud/scoreboard.ts
@@ -2,23 +2,36 @@ import { Engine, ScreenElement } from 'excalibur'
 import { assets } from '../assets'
 
 class ScoreboardNumber extends ScreenElement {
-  constructor({ x = 0, y = 0 }) {
+  constructor({ x = 0, y = 0, team = 'home' }) {
     super({
       x,
       y,
     })
-    this.graphics.use(assets.ase_scoreNumbers.getAnimation('0'))
+    this.team = team
+    this.score = 0
+    this.graphics.use(assets.ase_scoreNumbers.getAnimation(String(this.score)))
+  }
+
+  onInitialize() {
+    this.scene.on('goal', (event) => {
+      if (event.team === this.team) {
+        if (this.score < 9) {
+          this.score += 1
+        }
+        this.graphics.use(assets.ase_scoreNumbers.getAnimation(String(this.score)))
+      }
+    })
   }
 }
 
 class ScoreboardTeamBadge extends ScreenElement {
-  constructor({ x = 0, y = 0, team }) {
+  constructor({ x = 0, y = 0, team = 'home' }) {
     super({
       x,
       y,
     })
     this.graphics.use(
-      team === 'blue'
+      team === 'home'
         ? assets.img_blueTeamBadge.toSprite()
         : assets.img_redTeamBadge.toSprite()
     )
@@ -34,14 +47,14 @@ export class Scoreboard extends ScreenElement {
   }
 
   onInitialize(engine: Engine): void {
-    const homeScoreNumber = new ScoreboardNumber({ x: 0, y: 0 })
+    const homeScoreNumber = new ScoreboardNumber({ x: 0, y: 0, team: 'home' })
     engine.add(homeScoreNumber)
-    const homeBadge = new ScoreboardTeamBadge({ x: 36, y: 6, team: 'blue' })
+    const homeBadge = new ScoreboardTeamBadge({ x: 36, y: 6, team: 'home' })
     engine.add(homeBadge)
 
-    const awayScoreNumber = new ScoreboardNumber({ x: 270, y: 0 })
+    const awayScoreNumber = new ScoreboardNumber({ x: 270, y: 0, team: 'away' })
     engine.add(awayScoreNumber)
-    const awayBadge = new ScoreboardTeamBadge({ x: 268, y: 6, team: 'red' })
+    const awayBadge = new ScoreboardTeamBadge({ x: 268, y: 6, team: 'away' })
     engine.add(awayBadge)
   }
 }
